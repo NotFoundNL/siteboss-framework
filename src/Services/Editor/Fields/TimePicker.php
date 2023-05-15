965bf845f63255e92251b75aaa918a0856ec230c
Xander Schuurman <44030544+keeama13@users.noreply.github.com>
2023-05-15 14:28:06 +0200

    feat: timepicker (#313)

---
+++ src/Services/Editor/Fields/TimePicker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NotFound\Framework\Services\Editor\Fields;
+
+use NotFound\Framework\Services\Editor\Properties;
+
+class TimePicker extends Properties
+{
+    public function description(): string
+    {
+        return 'TimePicker';
+    }
+
+    public function properties(): void
+    {
+        $this->overview();
+        $this->sortable();
+        $this->required();
+    }
+
+    public function serverProperties(): void
+    {
+    }
+}
