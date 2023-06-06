3be47f622862d555cade390193be5a21a5444e3b
keeama13 <xander@schuurmannen.nl>
2023-06-06 14:20:34 +0200

    fix: namespace errors

--- src/Services/Indexer/IndexBuilderService.php
+++ src/Services/Indexer/IndexBuilderService.php
@@ -186,7 +186,7 @@ class IndexBuilderService
     private function updateSubPages($menu, $lang)
     {
         $class = $menu->template->filename ?? '';
-        $className = 'Siteboss\NotFound\Framework\Http\Controllers\Page\\'.$class.'Controller';
+        $className = 'App\Http\Controllers\Page\\'.$class.'Controller';
         $c = null;
 
         // update subPage if necessary
