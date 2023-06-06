4d2cb8149a6c8ad9bfbea566c00f245d0df19c80
Xander Schuurman <44030544+keeama13@users.noreply.github.com>
2023-06-06 14:04:16 +0200

    fix!: database prefix (#6)

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => env('DB_DATABASE_PREFIX', ''),
+            'prefix' => env('DB_PREFIX', ''),
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
