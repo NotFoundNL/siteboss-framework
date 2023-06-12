bbb54a3db26a29749947c7a343e76f88bb991d4b
Rene <rene@notfound.nl>
2023-06-12 14:36:59 +0200

    feat: send mail from helper

--- src/Helpers/SitebossHelper.php
+++ src/Helpers/SitebossHelper.php
@@ -37,4 +37,42 @@ class SitebossHelper
 
         return self::$config[$code]['value'];
     }
+
+    public static function mail(string $to_name, string $to_email, string $subject, $html, $data = false)
+    {
+        $sendgrid_api_key      = self::config('sendgrid_api_key', true);
+        $sendgrid_sender_email = self::config('sendgrid_sender_email', true);
+        $sendgrid_sender_name  = self::config('sendgrid_sender_name', true);
+    
+        $email = new \SendGrid\Mail\Mail();
+        $email->setFrom($sendgrid_sender_email, $sendgrid_sender_name);
+        $email->setSubject($subject);
+    
+        if (app()->hasDebugModeEnabled()) {
+            \env('SB_ERROR_EMAIL', $to_email);
+        } else {
+            if(filter_var($to_email, FILTER_VALIDATE_EMAIL)){
+                $email->addTo($to_email, $to_name);
+            } else {
+                return 400;
+            }
+        }
+    
+        // if ($data != false) {
+        //     // HTML is a Twig template with data
+        //     if(!site::$page->twig)
+        //     {
+        //         site::$page->getTwig(); 
+        //     }
+        //     $html = site::$page->twig->render($html, $data);
+        // }
+        $email->addContent("text/html", $html);
+        $sendgrid = new \SendGrid($sendgrid_api_key);
+        try {
+            $response = $sendgrid->send($email);
+            return $response->statusCode();
+        } catch (\Exception $e) {
+            echo 'Caught exception: ' . $e->getMessage() . "\n";
+        }
+    }
 }
