97d45cb3856064b7d634cd27f52a6874d96adf3c
Xander Schuurman <44030544+keeama13@users.noreply.github.com>
2023-06-13 10:54:49 +0200

    fix: mail forms (#19)

---
+++ src/Mail/Forms/ConfirmationEndUser.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace NotFound\Framework\Mail\Forms;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use NotFound\Framework\Models\Forms\Form;
+use NotFound\Framework\Models\Lang;
+use NotFound\Framework\Services\Forms\ValidatorInfo;
+
+class ConfirmationEndUser extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        protected Form $form,
+        protected ValidatorInfo $validatorInfo,
+        protected Lang $lang
+    ) {
+    }
+
+    public function build()
+    {
+        return $this->markdown('mail.formbuilder.confirmation-end-user')
+            ->subject($this->form->name)
+            ->with([
+                'form' => $this->form,
+                'html' => $this->convertSubmittedValuesToReadableValues(),
+            ]);
+    }
+
+    private function convertSubmittedValuesToReadableValues()
+    {
+        $langurl = $this->lang->url;
+        $pattern = '/<span.*?data-field=\"([0-9]+)\".*?>.*?<\/span>/i';
+        $validatorList = $this->validatorInfo->validators();
+        $confirmationEmail = $this->form->confirmation_mail->$langurl;
+
+        $output = preg_replace_callback($pattern, function ($matches) use ($validatorList, $langurl) {
+            $fieldId = $matches[1];
+            $fieldValue = '';
+
+            foreach ($validatorList as $validator) {
+                if ($validator->getFieldId() == $fieldId) {
+                    $fieldValue = $validator->getReadableValue($langurl);
+                }
+            }
+
+            return $fieldValue;
+        }, $confirmationEmail);
+
+        return $output;
+    }
+}
---
+++ src/Mail/Indexer/QueryError.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NotFound\Framework\Mail\Indexer;
+
+use Illuminate\Mail\Mailable;
+
+class QueryError extends Mailable
+{
+    /**
+     * Create a new message instance.
+     *
+     * @param  \NotFound\Framework\Models\Form  $form
+     * @return void
+     */
+    public function __construct(
+        private string $query,
+        private string $result,
+        private string $server
+    ) {
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('mail.indexer.query-error')
+            ->subject('Query error')
+            ->with([
+                'query' => $this->query,
+                'result' => $this->result,
+                'server' => $this->server,
+            ]);
+    }
+}
