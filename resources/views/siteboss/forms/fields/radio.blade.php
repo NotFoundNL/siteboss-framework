a17f35af7d514ab68035922a3031d8a61ffd7606
MAPCMC <maarten@notfound.nl>
2023-06-15 16:42:36 +0200

    fix: get string from nested option

--- resources/views/siteboss/forms/fields/radio.blade.php
+++ resources/views/siteboss/forms/fields/radio.blade.php
@@ -10,7 +10,7 @@
 
     <div>
         @foreach ($optionList as $option)
-            @php($optionString = $getByLanguage($option))
+            @php($optionString = $getByLanguage($option->option))
             <div class="form-check form-check-inline">
                 <input class="form-check-input"
                     type="radio"
