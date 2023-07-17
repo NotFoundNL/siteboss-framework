d78763b58778fd2a8fcc334cf52162fde91f089e
keeama13 <xander@schuurmannen.nl>
2023-07-17 14:06:12 +0200

    feat: added translations to verify email

--- lang/nl/auth.php
+++ lang/nl/auth.php
@@ -16,6 +16,9 @@ return [
     'throttle' => 'Te veel mislukte loginpogingen. Probeer het over :seconds seconden nogmaals.',
     'password' => 'Het opgegeven wachtwoord is onjuist.',
     'verify_email' => 'Verifieer uw e-mailadres.',
+    'verify_email_button' => 'Bevestig e-mailadres',
+    'verify_email_link' => 'Klik op onderstaande link om je e-mailadres te bevestigen.',
     'verify_email_resend' => 'Verificatiemail opnieuw verzenden',
     'verify_email_link_sent' => 'Er is een nieuwe verificatielink naar jouw e-mailadres verzonden.',
+    'verify_wrong_email' => 'Klik hier als dit bericht niet van jou komt.',
 ];
--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -2,14 +2,14 @@
 <x-mail::message>
 # {{ __('Welkom bij Veiligheids Coalitie') }}
 
-{{ __('Klik op onderstaande link om je e-mailadres te bevestigen.') }}
+{{ __('siteboss::auth.verify_email_link') }}
 
 <x-mail::button :url="$url">
-{{ __('Bevestig e-mailadres') }}
+{{ __('siteboss::auth.verify_email_button') }}
 </x-mail::button>
 
 Thanks,<br>
 {{ config('app.name') }}
 
-[{{__('Klik hier als jij dit niet was.')}}]({{$blockUrl}}) 
+[{{__('siteboss::auth.verify_wrong_email')}}]({{$blockUrl}}) 
 </x-mail::message>
