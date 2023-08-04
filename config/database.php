b5389544f467d9e0520f6d794d7dd650aa8b83b3
Rene <rene@64k.nl>
2023-08-04 15:18:08 +0200

    style :formatting

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
 
     'default' => env('DB_CONNECTION', 'mysql'),
     'prefix' => env('DB_PREFIX', ''),
-    
+
     /*
     |--------------------------------------------------------------------------
     | Database Connections
