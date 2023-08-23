5907254771156ce68c9c35b68a5bd2bdf46ac6d1
Thessa Kockelkorn <thessa@notfound.nl>
2023-08-23 13:40:49 +0200

    fix: laravel naming conventions

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use NotFound\Framework\Services\Indexer\IndexBuilderService;
 
-Artisan::command('siteboss:indexSite {--debug : Whether debug messages should be displayed}', function ($debug) {
+Artisan::command('siteboss:index-site {--debug : Whether debug messages should be displayed}', function ($debug) {
     //
     $indexer = new IndexBuilderService($debug);
     $indexer->run();
