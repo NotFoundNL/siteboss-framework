b04b87406bffbc754772cf884a0378cbcba9a8e7
Rene <rene@64k.nl>
2023-10-17 18:52:37 +0200

    feat: toggle TableItems

--- routes/cms/editor.php
+++ routes/cms/editor.php
@@ -28,6 +28,7 @@ Route::prefix('table')->group(function () {
         Route::prefix('{tableItem}')->group(function () {
             Route::get('', [CmsEditorTableItemController::class, 'readOne']);
             Route::post('', [CmsEditorTableItemController::class, 'update']);
+            Route::post('enabled', [CmsEditorTableItemController::class, 'enabled']);
         });
     });
 });
