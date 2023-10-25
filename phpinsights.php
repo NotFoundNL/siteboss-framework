59efbcc16605c272caf31e53b7f45a3ada04da4d
Rene <rene@64k.nl>
2023-10-25 14:26:13 +0200

    feat: update config

--- phpinsights.php
+++ phpinsights.php
@@ -8,17 +8,37 @@ return [
         //  'path/to/directory-or-file'
     ],
     'add' => [
-        //  ExampleMetric::class => [
-        //      ExampleInsight::class,
-        //  ]
+            Classes::class => [
+                ForbiddenFinalClasses::class,
+            ],
     ],
     'remove' => [
-        //  ExampleInsight::class,
+        AlphabeticallySortedUsesSniff::class,
+        DeclareStrictTypesSniff::class,
+        DisallowMixedTypeHintSniff::class,
+        ForbiddenDefineFunctions::class,
+        ForbiddenNormalClasses::class,
+        ForbiddenTraits::class,
+        ParameterTypeHintSniff::class,
+        PropertyTypeHintSniff::class,
+        ReturnTypeHintSniff::class,
+        UselessFunctionDocCommentSniff::class,
+        // Pint will remove this, so don't enforce it
+        SlevomatCodingStandard\Sniffs\TypeHints\NullableTypeForNullDefaultValueSniff::class,
     ],
     'config' => [
         \PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff::class => [
             'lineLimit' => 120,
-            'absoluteLineLimit' => 160
-        ]
+            'absoluteLineLimit' => 160,
+        ],
+        ForbiddenPrivateMethods::class => [
+            'title' => 'The usage of private methods is not idiomatic in Laravel.',
+        ],
+    ],
+    'requirements' => [
+        'min-quality' => 90,
+        'min-complexity' => 77,
+        'min-architecture' => 90,
+        'min-style' => 90,
     ],
 ];
