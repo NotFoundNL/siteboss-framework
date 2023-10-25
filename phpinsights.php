a273bdd0752990ae022d3199b5599a6ec874c369
Rene <rene@64k.nl>
2023-10-25 14:22:29 +0200

    feat: add config for insights

---
+++ phpinsights.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'preset' => 'laravel',
+    'exclude' => [
+        //  'path/to/directory-or-file'
+    ],
+    'add' => [
+        //  ExampleMetric::class => [
+        //      ExampleInsight::class,
+        //  ]
+    ],
+    'remove' => [
+        //  ExampleInsight::class,
+    ],
+    'config' => [
+        \PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff::class => [
+            'lineLimit' => 120,
+            'absoluteLineLimit' => 160
+        ]
+    ],
+];
