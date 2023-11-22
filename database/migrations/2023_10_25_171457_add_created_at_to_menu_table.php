e9c492dcd3da1775ec715e4e37c9cbabf30e6515
René <rene@notfound.nl>
2023-11-22 23:13:50 +0100

    fix: only add column when it doesn't exist

--- database/migrations/2023_10_25_171457_add_created_at_to_menu_table.php
+++ database/migrations/2023_10_25_171457_add_created_at_to_menu_table.php
@@ -4,13 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        if (Schema::hasColumn('menu', 'created_at')) {
+            return;
+        }
         Schema::table('menu', function (Blueprint $table) {
             $table->timestamp('created_at')->nullable();
         });
