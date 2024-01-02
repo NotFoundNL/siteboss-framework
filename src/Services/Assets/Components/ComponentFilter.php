f1fd0713bfbaccbdbc14942b5a619fabeda8eeef
keeama13 <xander@schuurmannen.nl>
2024-01-02 12:27:20 +0100

    feat: component filter added

---
+++ src/Services/Assets/Components/ComponentFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NotFound\Framework\Services\Assets\Components;
+
+use NotFound\Layout\Elements\AbstractLayout;
+use NotFound\Layout\Inputs\LayoutInputHidden;
+
+class ComponentFilter extends AbstractComponent
+{
+    protected bool $useDefaultStorageMechanism = false;
+
+    public function getAutoLayoutClass(): ?AbstractLayout
+    {
+        return new LayoutInputHidden($this->assetItem->internal, $this->assetItem->name);
+    }
+
+    public function validate($newValue): bool
+    {
+        // TODO: Implement validate() method.
+        return true;
+    }
+}
