52c0a9c79d3094866ba85a23299e34deb3b11134
Rene <rene@64k.nl>
2024-01-09 18:22:37 +0100

    fix: do not crash on routing error

--- src/Services/PageRouterService.php
+++ src/Services/PageRouterService.php
@@ -4,6 +4,7 @@ namespace NotFound\Framework\Services;
 
 use Exception;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath;
@@ -50,7 +51,7 @@ class PageRouterService
             if (isset($route->children) && isset($route->children[0])) {
                 $this->setPageAsRoute($route->children[0], true);
             } else {
-                dd('No child route set for site');
+                Log::error('No child route set for site');
             }
 
             foreach ($route->children as $page) {
