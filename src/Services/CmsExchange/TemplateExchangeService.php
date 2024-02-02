6cc299e80390321e0688d0abb41115a718f6f4db
René <rene@64k.nl>
2024-02-02 16:47:06 +0100

    feat: Version 0.17.0 (#195)
    * feat: component filter added (#176)
    
    * fix: only add column when it doesn't exist (#167)
    
    * fix: only add column when it doesn't exist
    
    * style: formatting
    
    * feat: export cms (#169)
    
    * feat: import export
    
    * feat: use file instead of database
    
    * feat: move import/export to model
    
    * fix: export from tableitem
    
    * feat: move export to Trait
    
    * style: formatting
    
    * style: formatting
    
    * fix: never use file as source of truth
    
    * style: formatting
    
    * chore: cleanup
    
    * fix: use export to file
    
    * feat: comment things that no longer work
    
    * feat: add  meta data
    
    * feat: list filenames
    
    * feat: cleaner export
    
    * feat: remove UI for import/export
    
    * style: formatting
    
    * feat: add import command
    
    * feat: import from files
    
    * feat: remove import UI
    
    * feat: prevent editing tables with changes on disk
    
    * feat: do not use id's in export for tables
    
    * feat: optionally export ids
    
    * feat: import ids when available
    
    ---------
    
    Co-authored-by: Rene <rene@64k.nl>
    
    * feat: component filter added
    
    * feat: added filterparams
    
    * feat: only query if filter exists
    
    * feat: WIP
    
    * feat: typed editor classes
    
    * feat: editor changes
    
    * feat: removed code
    
    * feat: removed request from editor and overview
    
    * feat: added TODO
    
    * chore: update dependency
    
    * feat: migration for model column in cms_table
    
    * fix: retain model on import export cms_table
    
    * feat: moved code to component
    
    * feat: added guards
    
    * style: formatting
    
    * feat: parameters now in tableservice
    
    * feat: removed parameter
    
    * feat: removed request() from editor
    
    * fix: return empty array if key not exists
    
    * style: formatting
    
    * style: formatting
    
    ---------
    
    Co-authored-by: René <rene@64k.nl>
    
    * fix: exceptions handling (#189)
    
    * fix: exception handling
    
    * style: formatting
    
    * fix: return type
    
    * feat: date helper (#191)
    
    * feat: moved layoutbar to editor (#193)
    
    * feat: moved layoutbar to editor
    
    * style: formatting
    
    * chore: remove unused methods
    
    ---------
    
    Co-authored-by: Rene <rene@64k.nl>
    
    * feat: CMS editor import/export (#197)
    
    * feat: move to service
    
    * style: formatting
    
    * feat: template import
    
    * fix: edit tables
    
    * feat: moved bar items to editor (#196)
    
    * feat: moved bar items to editor
    
    * feat: abstract class
    
    * style: formatting
    
    * feat: updated default editor
    
    ---------
    
    Co-authored-by: Rene <rene@64k.nl>
    
    ---------
    
    Co-authored-by: Xander Schuurman <44030544+keeama13@users.noreply.github.com>

---
+++ src/Services/CmsExchange/TemplateExchangeService.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace NotFound\Framework\Services\CmsExchange;
+
+use File;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use NotFound\Framework\Models\Table;
+use NotFound\Framework\Models\Template;
+use NotFound\Framework\Models\TemplateItem;
+
+class TemplateExchangeService extends AbstractExchangeService
+{
+    protected string $exportTypeName = 'template';
+
+    public function runImport(): void
+    {
+        $this->debug('Starting CMS Template Import');
+        $this->importTemplates();
+    }
+
+    public function hasChanges(Template $table): bool
+    {
+        // TODO: Implement hasChanges() method + call it when updating templates
+        $path = resource_path('siteboss/templates/'.$table->filename.'.json');
+        if (! File::exists($path)) {
+            return false;
+        }
+        $data = $table->exportToObject();
+        $fileData = json_decode(file_get_contents($path));
+
+        return $data != $fileData;
+    }
+
+    private function importTemplates(): object
+    {
+        $path = resource_path('siteboss/templates');
+        if (! File::exists($path)) {
+            $this->debug('No export files found in '.$path);
+
+            return (object) [];
+        }
+
+        // Create temp table to import into
+        $this->createImportTables();
+
+        $fileSources = [];
+        $files = File::files($path);
+        foreach ($files as $file) {
+            $index = str_replace('.json', '', $file->getFilename());
+            $fileSources[$index] = json_decode(file_get_contents($file->getPathname()));
+        }
+        $this->debug('== Read '.count($fileSources).' files from '.$path);
+
+        $order = 1;
+        foreach ($fileSources as $tableName => $fileSource) {
+            if ($this->dryRun) {
+                $this->debug('CREATE TABLE '.$tableName);
+            } else {
+                $table = new Template();
+                $table->id = $fileSource->id;
+                $table->name = $fileSource->name;
+                $table->filename = $fileSource->filename;
+                $table->rights = $fileSource->rights;
+
+                $table->allow_children = $fileSource->allow_children;
+                $table->params = $fileSource->params;
+                $table->desc = $fileSource->desc;
+                $table->properties = $fileSource->properties;
+                $table->order = $order++;
+                $table->enabled = $fileSource->enabled;
+                $table->save();
+            }
+            $itemOrder = 1;
+            foreach ($fileSource->items as $item) {
+                if ($this->dryRun) {
+                    $this->debug(' [x] '.$item->name);
+
+                    continue;
+                }
+                $tableItem = new TemplateItem();
+                if (isset($item->id)) {
+                    $tableItem->id = $item->id;
+                }
+                $tableItem->template = $table->id;
+                $tableItem->name = $item->name;
+                $tableItem->type = $item->type;
+                $tableItem->internal = $item->internal;
+                $tableItem->description = $item->description;
+                $tableItem->properties = $item->properties;
+                $tableItem->server_properties = $item->server_properties;
+                $tableItem->order = $itemOrder++;
+                $tableItem->enabled = $item->enabled;
+                $tableItem->rights = $item->rights;
+
+                $tableItem->save();
+
+            }
+
+        }
+
+        return (object) [];
+    }
+
+    private function createImportTables(): void
+    {
+        if ($this->dryRun) {
+            $this->debug('Dry run: skipping table creation');
+
+            return;
+        }
+        $this->debug('Creating import tables');
+        Schema::dropIfExists('cms_template_backup');
+        Schema::rename('cms_template', 'cms_template_backup');
+        Schema::create('cms_template', function (Blueprint $table) {
+            $table->id();
+            $table->string('rights', 128)->nullable();
+            $table->string('name', 128)->nullable();
+            $table->string('desc', 255)->nullable();
+            $table->string('filename', 128)->nullable();
+            $table->string('allow_children', 128)->nullable();
+            $table->string('params', 128)->nullable();
+            $table->json('properties')->nullable();
+            $table->integer('order')->nullable();
+            $table->tinyInteger('enabled')->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::dropIfExists('cms_templateitem_backup');
+        Schema::rename('cms_templateitem', 'cms_templateitem_backup');
+        Schema::create('cms_templateitem', function (Blueprint $table) {
+            $table->id();
+            $table->string('rights', 128)->default('');
+            $table->foreignIdFor(Template::class, 'template')->nullable();
+            $table->string('type', 64)->nullable();
+            $table->string('internal', 64)->nullable();
+            $table->string('name', 128)->nullable();
+            $table->text('description')->nullable();
+            $table->json('properties')->nullable();
+            $table->json('server_properties')->nullable();
+            $table->boolean('global')->default(0);
+            $table->integer('order')->nullable();
+            $table->tinyInteger('enabled')->nullable()->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+    }
+
+    public function exportTypeName(): string
+    {
+        return 'template';
+    }
+
+    public function exportRetainIds(): bool
+    {
+        return true;
+    }
+}
