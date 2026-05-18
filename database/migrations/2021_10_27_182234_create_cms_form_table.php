98171744e694e3301ef38033a913ae47ea820261
Rene <rene@64k.nl>
2026-05-18 15:51:28 +0200

    fix: make migrations anonymous

--- database/migrations/2021_10_27_182234_create_cms_form_table.php
+++ database/migrations/2021_10_27_182234_create_cms_form_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use NotFound\Framework\Models\Forms\Category;
 
-class CreateCmsFormTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -35,4 +35,4 @@ class CreateCmsFormTable extends Migration
     {
         Schema::dropIfExists('cms_form');
     }
-}
+};
